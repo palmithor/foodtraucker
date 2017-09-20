0e9dce8fb62d66d3a3344c04416b75ccb7354917
Pálmi Þór Valgeirsson <palmi.valgeirsson@cygni.se>
2017-09-21 00:11:41 +0200

    #5 Add environment variables for user pool

--- backend_java/src/main/java/com/foodtraucker/serverless/users/SignupHandler.java
+++ backend_java/src/main/java/com/foodtraucker/serverless/users/SignupHandler.java
@@ -10,16 +10,19 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SignupHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = Logger.getLogger(SignupHandler.class);
+    private static final Logger LOG = Logger.getLogger(SignupHandler.class);
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("This will sign up a user", input);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-				.build();
-	}
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: " + input);
+        Response responseBody = new Response("This will sign up a user", input);
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(responseBody)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                .build();
+    }
 }
+
+
+// todo http://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-pools-social.html
