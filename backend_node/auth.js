ef041bc59d723c1444c191492bd0c0da4008cd9d
Semone Kallin Thander <semoneclarke@gmail.com>
2017-10-02 22:02:55 +0200

    added node_modules to .gitignore serverless.yml is now creating two tables one for users and one for traucks lamdas have permission to access dynamo tables

--- backend_node/auth.js
+++ backend_node/auth.js
@@ -3,6 +3,10 @@
 //const uuid = require('uuid');
 //const dynamodb = require('./dynamodb');
 
+//const userTable = process.env.USERS_TABLE;
+//const traucksTable = process.env.TRAUCKS_TABLE;
+
+
 module.exports.signup = (event, context, callback) => {
     console.log('hello world');
     callback(null, {message: "OK"})
