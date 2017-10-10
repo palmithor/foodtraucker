443b786c95c3907f24b6b408bdd1d098ad64c713
Pálmi Þór Valgeirsson <palmi.valgeirsson@cygni.se>
2017-10-10 17:46:20 +0200

    #5 Finalize signup function

--- backend_node/auth.js
+++ backend_node/auth.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var aws = require('aws-sdk');
+var dynamodb = new aws.DynamoDB();
+
+
 
 //const uuid = require('uuid');
 //const dynamodb = require('./dynamodb');
@@ -9,7 +12,51 @@ var aws = require('aws-sdk');
 //const traucksTable = process.env.TRAUCKS_TABLE;
 
 
-module.exports.signup = function(event, context) {
+module.exports.signup = function (event, context) {
     console.log('Event: ' + JSON.stringify(event));
-    context.done(null, event);
+    console.log('NODE_ENV = ' + process.env.NODE_ENV);
+
+    var userAuthTableName = 'foodtraucker-' + process.env.NODE_ENV + '-user-auth';
+    var userTableName = 'foodtraucker-' + process.env.NODE_ENV + '-users';
+
+
+    var datetime = new Date().getTime().toString();
+
+    dynamodb.listTables().promise()
+        .then(function (result) {
+            console.log(JSON.stringify(result, null, '  '));
+            return dynamodb.putItem({
+                "TableName": userAuthTableName,
+                "Item": {
+                    "cognito_id": {
+                        "S": event.userName
+                    },
+                    "date": {
+                        "S": datetime
+                    },
+                    "email": {
+                        "S": event.request.userAttributes.email
+                    }
+                }
+            }).promise();
+        }).then(function (result) {
+            return dynamodb.putItem({
+                "TableName": userTableName,
+                "Item": {
+                    "email": {
+                        "S": event.request.userAttributes.email
+                    },
+                    "date": {
+                        "S": datetime
+                    }
+                }
+            }).promise();
+        })
+        .then(function (result) {
+            console.log('Success YAY!')
+            context.done(null, event);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 };
--- package_backend.sh
+++ package_backend.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+node_backend_package="./backend_node.zip"
+if [ -f "$node_backend_package" ]
+then
+	rm $node_backend_package
+fi
+
 zip -j backend_node.zip backend_node/*
 mvn clean package -f backend_java/pom.xml
