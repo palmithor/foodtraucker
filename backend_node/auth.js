b4cbd717e0f8498e0fc8c27ef294e50e55c2b7ba
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2017-10-10 21:04:31 +0200

    #5 remove user auth table as it is unnecessary

--- backend_node/auth.js
+++ backend_node/auth.js
@@ -3,43 +3,16 @@
 var aws = require('aws-sdk');
 var dynamodb = new aws.DynamoDB();
 
-
-
-//const uuid = require('uuid');
-//const dynamodb = require('./dynamodb');
-
-//const userTable = process.env.USERS_TABLE;
-//const traucksTable = process.env.TRAUCKS_TABLE;
-
+const userTableName = process.env.USERS_TABLE;
 
 module.exports.signup = function (event, context) {
     console.log('Event: ' + JSON.stringify(event));
-    console.log('NODE_ENV = ' + process.env.NODE_ENV);
-
-    var userAuthTableName = 'foodtraucker-' + process.env.NODE_ENV + '-user-auth';
-    var userTableName = 'foodtraucker-' + process.env.NODE_ENV + '-users';
-
 
     var datetime = new Date().getTime().toString();
 
     dynamodb.listTables().promise()
         .then(function (result) {
             console.log(JSON.stringify(result, null, '  '));
-            return dynamodb.putItem({
-                "TableName": userAuthTableName,
-                "Item": {
-                    "cognito_id": {
-                        "S": event.userName
-                    },
-                    "date": {
-                        "S": datetime
-                    },
-                    "email": {
-                        "S": event.request.userAttributes.email
-                    }
-                }
-            }).promise();
-        }).then(function (result) {
             return dynamodb.putItem({
                 "TableName": userTableName,
                 "Item": {
@@ -50,7 +23,7 @@ module.exports.signup = function (event, context) {
                         "S": datetime
                     }
                 }
-            }).promise();
+            }).promise();    
         })
         .then(function (result) {
             console.log('Success YAY!')
