d230822a6dcf96d8bd709659a1757e7c3491f8de
Pálmi Þór Valgeirsson <palmi.valgeirsson@cygni.se>
2017-10-12 18:50:19 +0200

    Remove # from urls

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: Hello,
+      props: (route) => ({ appAwsCode: route.query.code })
     }
+    /* eslint-disable*/
+    // todo { path: '*', component: NotFoundComponent }
   ]
 })
