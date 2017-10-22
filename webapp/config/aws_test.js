6623a7e38fb6bf69850b752293871d9637b49425
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2017-10-22 23:31:06 +0200

    started configuring aws configurations for auth

--- webapp/config/aws_test.js
+++ webapp/config/aws_test.js
@@ -1,4 +1,8 @@
 module.exports = {
-    NODE_ENV: '"aws_test"'
+    NODE_ENV: '"aws_test"',
+    USER_POOL_ID: '"eu-central-1_RxnIPPZui"',
+    AWS_REGION: '"eu-central-1"',
+    COGNITO_CLIENT_ID: '"vqjsnpiojek70s79t4m1kt8li"',
+    IDENTITY_POOL_ID: '"eu-central-1:a88c79e9-c177-4e7d-95ef-925fcdf027df"'
   }
   
