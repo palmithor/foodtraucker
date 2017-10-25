bd911cb665ff760c678e67f59778b9ad83c94554
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2017-10-26 00:56:40 +0200

    #24 added configurations for aws_test profile and added dependencies for auth

--- webapp/config/aws_test.js
+++ webapp/config/aws_test.js
@@ -4,5 +4,4 @@ module.exports = {
     AWS_REGION: '"eu-central-1"',
     COGNITO_CLIENT_ID: '"vqjsnpiojek70s79t4m1kt8li"',
     IDENTITY_POOL_ID: '"eu-central-1:a88c79e9-c177-4e7d-95ef-925fcdf027df"'
-  }
-  
+  };
