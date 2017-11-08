7c324e544d6074c787faf79efa3aaf9259fe0299
Pálmi Þór Valgeirsson <palmithor@users.noreply.github.com>
2017-11-08 13:30:45 +0100

    Webapp Login / Signup (#31)
    * started configuring aws configurations for auth
    
    * #24 added configurations for aws_test profile and added dependencies for auth
    
    * #25 : Added auth service with login, signup, forgot password and confirm password as well as started implementing login component.
    
    * #25 signup and login now functioning with validation
    
    * #25 : Fix tests and merge materialize
    
    * #25 export identity pool id
    
    * #25 fix login validation
    
    * #25 add minor changes to session and aws profile

--- webapp/config/dev.env.js
+++ webapp/config/dev.env.js
@@ -2,5 +2,9 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  USER_POOL_ID: '"eu-central-1_RxnIPPZui"',
+  AWS_REGION: '"eu-central-1"',
+  COGNITO_CLIENT_ID: '"vqjsnpiojek70s79t4m1kt8li"',
+  IDENTITY_POOL_ID: '"eu-central-1:a88c79e9-c177-4e7d-95ef-925fcdf027df"'
 })
--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    <app-nav-bar/>
+    <div>
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import NavBar from '@/components/shared/NavBar'
+
+  export default {
+    components: {
+      appNavBar: NavBar
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
