3a03ce0e7b679a7613ce9d950830c00339d6fad3
Pálmi Þór Valgeirsson <palmi.valgeirsson@cygni.se>
2018-03-02 17:18:58 +0100

    Yarn workspaces fix #5 fix backend package

--- package_backend.sh
+++ package_backend.sh
@@ -11,6 +11,6 @@ npm --prefix ./backend_node install ./backend_node
 
 cd backend_node; zip -r ../backend_node.zip *; cd ..
 
-cd backend_node_2; yarn install; yarn test; yarn build; yarn package
+cd backend_node_2; yarn install; yarn test; yarn build; yarn package; cd ..
 
 mvn clean package -f backend_java/pom.xml
